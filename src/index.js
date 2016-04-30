0b7a6d253652c469c60d63de069531e8c77f4b13
Anton Kuznetsov <isnifer@gmail.com>
2016-04-30 15:41:01 +0300

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+class AwesomeComponent extends Component {
+    state = {
+        body: 'Awesome Component',
+    }
+
+    render() {
+        return (
+            <div className="awesome-component">
+                {this.state.body}
+            </div>
+        );
+    }
+}
+
+render(React.createElement(AwesomeComponent), document.getElementById('react-root'));
---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+var path = require('path');
+var webpack = require('webpack');
+
+
+var ENV = process.env;
+var PRODUCTION = ENV.NODE_ENV == 'production';
+var devtool = PRODUCTION ? false : 'eval';
+var entry, plugins;
+
+if (PRODUCTION){
+    entry = ['./src/index'];
+    plugins = [
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: JSON.stringify(ENV.NODE_ENV)
+            }}),
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.optimize.UglifyJsPlugin({
+            sourceMap: false,
+            compressor: {
+                warnings: false,
+                drop_console: true,
+                screw_ie8: true
+            }
+        })
+    ];
+} else {
+    entry = [
+        'webpack-hot-middleware/client',
+        './src/index'
+    ];
+    plugins = [
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin(),
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: JSON.stringify(ENV.NODE_ENV) || 'development'
+            },
+        })
+    ];
+}
+
+module.exports = {
+    devtool: devtool,
+    entry: entry,
+    plugins: plugins,
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'index.js',
+        publicPath: '/build/'
+    },
+    module: {
+        loaders: [
+            {test: /\.jsx?$/, loaders: ['babel'], include: path.join(__dirname, 'src')},
+            {test: /\.scss$/, loaders: ['style', 'css', 'less'], include: path.join(__dirname, 'less')}
+        ]
+    },
+    resolve: ['', 'js', 'jsx', 'sass']
+};
