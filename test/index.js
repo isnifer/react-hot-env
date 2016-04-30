ac4eb4b335afc3a682d5aaf5a9ea8b3b973ff8d0
Anton Kuznetsov <isnifer@gmail.com>
2016-04-30 17:55:39 +0300

    Update test

--- test/index.js
+++ test/index.js
@@ -7,7 +7,6 @@ import AwesomeComponent from '../lib/AwesomeComponent.js';
 
 test('Awesome Component', t => {
     const wrapper = shallow(<AwesomeComponent />);
-    console.log(wrapper);
     t.equal(wrapper.node.type, 'div', `Root tag should be 'div'`);
     t.equal(wrapper.node.props.className, 'awesome-component', `ClassName should be 'awesome-component'`);
     t.equal(wrapper.length, 1, `There is should be one component`);
