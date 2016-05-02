da865989ee49a7df9a0e648546296912ebeefa41
Anton Kuznetsov <isnifer@gmail.com>
2016-05-02 19:03:54 +0300

    Fix import on buidl

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import '../less/_main.less';
 import React from 'react';
 import { render } from 'react-dom';
-import AwesomeComponent from './components/AwesomeComponent';
+import AwesomeComponent from './components/AwesomeComponent.jsx';
 
 render(React.createElement(AwesomeComponent), document.getElementById('react-root'));
